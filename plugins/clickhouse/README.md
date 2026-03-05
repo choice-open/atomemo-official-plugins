1ab055ba7d65a745bb8c957155df514aed9b8eb4
leo.yu <leo.yu@choiceform.com>
2026-03-05 17:47:21 +0800

    🎉 init: 初始化插件

---
+++ plugins/clickhouse/README.md
@@ -0,0 +1,21 @@
+# Atomemo Plugin - clickhouse
+
+## Development
+
+- Install dependencies:
+
+```bash
+bun install
+```
+
+- Run the unit tests:
+
+```bash
+bun run test
+```
+
+- Build the library:
+
+```bash
+bun run build
+```
---
+++ plugins/clickhouse/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown"
+
+export default defineConfig({
+  dts: true,
+  entry: ["src/index.ts"],
+  exports: {
+    devExports: false,
+  },
+  format: "esm",
+  inlineOnly: ["typesafe-i18n"],
+  sourcemap: true,
+})
